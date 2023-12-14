376a65bc284dfd3422b2260d2c7a2af4420a7ac5
Mukesh Kumar <ak9991927952@gmail.com>
2023-12-14 21:15:27 +0530

    Update Jest version and add CRUD endpoints to todoServer.js

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -41,9 +41,63 @@
  */
   const express = require('express');
   const bodyParser = require('body-parser');
-  
   const app = express();
   
   app.use(bodyParser.json());
+
+  let todos = [];
+
+  app.get("/todos", (req, res) => {
+    res.json(todos);
+  });
+
+  app.get("/todos/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = todos.find((data) => data.id == id);
+    
+    if (!todo) {
+      res.status(404).send();
+      return;
+    }
+    
+    res.json(todo);
+  });
+
+  app.post("/todos", (req, res) => {
+    const todo = {
+      id: Math.floor(Math.random() * 1000000),
+      title: req.body.title,
+      completed: req.body.completed,
+      description: req.body.description,
+    };
+    
+    todos.push(todo);
+    res.status(201).json(todo);
+  });
+
+  app.put("/todos/:id", (req, res) => {
+    const todoIdex = todos.findIndex(data => data.id == req.params.id);
+    
+    if (todoIdex === -1) {
+      res.status(404).send();
+    } else {
+      todos[todoIdex].title = req.body.title;
+      todos[todoIdex].description = req.body.description;
+      res.json(todos[todoIdex]);
+    }
+  });
+
+  app.delete("/todos/:id", (req, res) => {
+    const todoIdex = todos.findIndex((data) => data.id == req.params.id);
+    
+    if (todoIdex === -1) {
+      res.status(404).send();
+    } else {
+      todos.splice(todoIdex, 1);
+      res.status(200).send();
+    }
+  });
+
+  module.exports = app;
   
   module.exports = app;
